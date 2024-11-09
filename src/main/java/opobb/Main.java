1ba817af1c15cf1927176e7457fc750d142eeb4f
Petteri <26197131+PetteriM1@users.noreply.github.com>
2024-11-09 15:21:43 +0200

    Update (any block, any resistance)

--- src/main/java/opobb/Main.java
+++ src/main/java/opobb/Main.java
@@ -2,22 +2,30 @@ package opobb;
 
 import cn.nukkit.entity.item.EntityPrimedTNT;
 import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.entity.EntityExplosionPrimeEvent;
 import cn.nukkit.plugin.PluginBase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main extends PluginBase implements Listener {
+
+    static Map<Integer, Double> resistance;
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+        resistance = getConfig().get("resistance", new HashMap<>());
         getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onBigBoom(EntityExplosionPrimeEvent e) {
-        if (!e.isCancelled() && e.getEntity() instanceof EntityPrimedTNT) {
+        if (e.getEntity() instanceof EntityPrimedTNT) {
             e.setCancelled(true);
-            BigExplosion explosion = new BigExplosion(e.getEntity(), (float) e.getForce() - 0.1, e.getEntity());
+            BigExplosion explosion = new BigExplosion(e.getEntity(), 4, e.getEntity());
             if (explosion.explodeA()) {
                 explosion.explodeB();
             }
